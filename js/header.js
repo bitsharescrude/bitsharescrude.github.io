7de3d3416be6189c61e68beb63aea53bc5a8ecd1
Crude Finance <bitsharescrude@gmail.com>
2023-07-30 18:53:29 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ console.log("############################################");
 const _NETWORK_ID = 369;
 let _CONTRACT_DATA = {};
 
-const _API_URL = 'https://scan.pulsechain.com/api'
+const _API_URL = 'https://7yjhmmd390.execute-api.ap-south-1.amazonaws.com/prod'
 
 _CONTRACT_DATA[_NETWORK_ID] = {
     network_name: "PulseChain",
